d828af07977a5e2cbce46568bfcd058ed74cba04
biswassupriya <supriya0483@gmail.com>
2020-08-02 17:34:37 +0100

    Initial Commit

--- src/main/java/com/coverage/analyzer/CoverageTracker.java
+++ src/main/java/com/coverage/analyzer/CoverageTracker.java
@@ -1,7 +1,9 @@
-/*
 package com.coverage.analyzer;
 
 import japa.parser.ASTHelper;
+import japa.parser.JavaParser;
+import japa.parser.ParseException;
+import japa.parser.ast.CompilationUnit;
 import japa.parser.ast.expr.IntegerLiteralExpr;
 import japa.parser.ast.expr.MethodCallExpr;
 import japa.parser.ast.expr.NameExpr;
@@ -9,6 +11,10 @@ import japa.parser.ast.expr.StringLiteralExpr;
 import japa.parser.ast.stmt.ExpressionStmt;
 import japa.parser.ast.stmt.Statement;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,8 +23,6 @@ public class CoverageTracker {
     private static Map<String, Map<Integer, Boolean>> coverage =
             new HashMap<String, Map<Integer, Boolean>>();
 
-    // Serializes coverage in some format; we'll revisit this.
-    public static void writeCoverageToFile() { }
 
     public static void markExecuted(String filename, int line) {
         if (!coverage.containsKey(filename)) {
@@ -29,10 +33,18 @@ public class CoverageTracker {
 
     static {
         Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override public void run() {
+            @Override
+            public void run() {
                 writeCoverageToFile();
             }
         });
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+        File file = new File(args[0]);
+        CompilationUnit unit = JavaParser.parse(new FileReader(file));
+        unit.accept(new CoverageVisitor(file.getAbsolutePath()), null);
+        System.out.println(unit.toString());
     }
 
     private Statement makeCoverageTrackingCall(String filename, int line) {
@@ -50,5 +62,35 @@ public class CoverageTracker {
         }
         coverage.get(filename).put(line, false);
     }
+
+
+    private static void writeCoverageToFile() {
+        String lcovCoverage = generateLcov();
+        String coverageReportPath = System.getProperty("coverage.report.path", "coverage_report.lcov");
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(coverageReportPath);
+            writer.write(lcovCoverage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static String generateLcov() {
+        StringBuilder sb = new StringBuilder();
+        for (String filename : coverage.keySet()) {
+            sb.append("SF:" + filename + "\n");
+            for (Map.Entry<Integer, Boolean> line : coverage.get(filename).entrySet()) {
+                sb.append(String.format("DA:%d,%d\n", line.getKey(), line.getValue() ? 1 : 0));
+            }
+            sb.append("end_of_record\n");
+        }
+        return sb.toString();
+    }
 }
-*/
--- src/main/java/com/coverage/analyzer/CoverageVisitor.java
+++ src/main/java/com/coverage/analyzer/CoverageVisitor.java
@@ -14,33 +14,95 @@ import japa.parser.ast.stmt.ExpressionStmt;
 import japa.parser.ast.stmt.Statement;
 import japa.parser.ast.visitor.ModifierVisitorAdapter;
 
-import java.io.StringReader;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CoverageVisitor extends ModifierVisitorAdapter<Object> {
-  // AST nodes don't know which file they come from, so we'll pass the information in
-  private String filename;
+    // AST nodes don't know which file they come from, so we'll pass the information in
+    private String filename;
 
-  public CoverageVisitor(String filename) {
-    this.filename = filename;
-  }
+    // Maps filenames and line numbers to true (executed) or false (not executed).
+    private static Map<String, Map<Integer, Boolean>> coverage =
+            new HashMap<String, Map<Integer, Boolean>>();
 
-  @Override
-  public Node visit(ExpressionStmt node, Object arg) {
-    BlockStmt block = new BlockStmt();
-    ASTHelper.addStmt(block, node);
-    ASTHelper.addStmt(block, makeCoverageTrackingCall(filename, node.getBeginLine()));
-    return block;
-  }
+    public CoverageVisitor(String filename) {
+        this.filename = filename;
+    }
 
-  private Statement makeCoverageTrackingCall(String filename, int line) {
-    NameExpr coverageTracker = ASTHelper.createNameExpr("com.coverage.analyzer.runtime.CoverageTracker");
-    MethodCallExpr call = new MethodCallExpr(coverageTracker, "markExecuted");
-    ASTHelper.addArgument(call, new StringLiteralExpr(filename));
-    ASTHelper.addArgument(call, new IntegerLiteralExpr(String.valueOf(line)));
-    return new ExpressionStmt(call);
-  }
+    @Override
+    public Node visit(ExpressionStmt node, Object arg) {
+        BlockStmt block = new BlockStmt();
+        ASTHelper.addStmt(block, node);
+        ASTHelper.addStmt(block, makeCoverageTrackingCall(filename, node.getBeginLine()));
+        return block;
+    }
 
-  public static void main(String[] args) throws ParseException {
+
+    private Statement makeCoverageTrackingCall(String filename, int line) {
+        CoverageVisitor.markExecutable(filename, line);
+        NameExpr coverageTracker = ASTHelper.createNameExpr("com.coverage.analyzer.runtime.CoverageTracker");
+        MethodCallExpr call = new MethodCallExpr(coverageTracker, "markExecuted");
+        ASTHelper.addArgument(call, new StringLiteralExpr(filename));
+        ASTHelper.addArgument(call, new IntegerLiteralExpr(String.valueOf(line)));
+        return new ExpressionStmt(call);
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+        File file = new File(args[0]);
+        CompilationUnit unit = JavaParser.parse(new FileReader(file));
+        unit.accept(new CoverageVisitor(file.getAbsolutePath()), null);
+        System.out.println(unit.toString());
+    }
+
+
+    public static void markExecutable(String filename, int line) {
+        if (!coverage.containsKey(filename)) {
+            coverage.put(filename, new HashMap<Integer, Boolean>());
+        }
+        coverage.get(filename).put(line, false);
+    }
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                writeCoverageToFile();
+            }
+        });
+    }
+
+    private static void writeCoverageToFile() {
+        String lcovCoverage = generateLcov();
+        String coverageReportPath = System.getProperty("coverage.report.path", "coverage_report.lcov");
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(coverageReportPath);
+            writer.write(lcovCoverage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static String generateLcov() {
+        StringBuilder sb = new StringBuilder();
+        for (String filename : coverage.keySet()) {
+            sb.append("SF:" + filename + "\n");
+            for (Map.Entry<Integer, Boolean> line : coverage.get(filename).entrySet()) {
+                sb.append(String.format("DA:%d,%d\n", line.getKey(), line.getValue() ? 1 : 0));
+            }
+            sb.append("end_of_record\n");
+        }
+        return sb.toString();
+    }
+
+/*  public static void main(String[] args) throws ParseException {
     CompilationUnit unit = JavaParser.parse(new StringReader(new StringBuilder()
             .append("package com.coverage.analyzer;\n\n")
             .append("public class Hello {\n")
@@ -53,7 +115,7 @@ public class CoverageVisitor extends ModifierVisitorAdapter<Object> {
     unit.accept(new CoverageVisitor("/path/to/Hello.java"), null);
 
     System.out.println(unit.toString());
-  }
+  }*/
 
 
 }
